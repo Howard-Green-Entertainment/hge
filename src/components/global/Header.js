6f7eeafaf6a04fa41ca47c5f688bd7db7e827d41
Ben Parisot <me@benparisot.com>
2019-10-14 19:55:48 -0700

    write basic header/footer components

---
+++ src/components/global/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <>
+            <section className="header">
+                <h1>Howard Green Entertainment</h1>
+            </section>
+        </>
+
+    )
+
+} 
