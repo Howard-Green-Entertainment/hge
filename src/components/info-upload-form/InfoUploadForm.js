b1aea878fadcc52df04337feb4b33b851f617b36
Ben Parisot <me@benparisot.com>
2019-10-14 20:15:30 -0700

    write additional form fields

--- src/components/info-upload-form/InfoUploadForm.js
+++ src/components/info-upload-form/InfoUploadForm.js
@@ -3,9 +3,19 @@ import React from 'react';
 export default function InfoUploadForm({ onSubmit }) {
     return (
         <>
-            <form onClick={() => onSubmit()}>
-                <input type="text" name="first name"></input>
-                <button>Submit</button>
+            <form>
+                <input type="text" name="first-name" defaultValue="First Name"></input>
+                <input type="text" name="last-name" defaultValue="Last Name"></input>
+                <input type="textarea" name="bio" defaultValue="Bio"></input>
+                <input type="text" name="external-link" defaultValue="External Link"></input>
+                <input type="text" name="external-link-title" defaultValue="External link title"></input>
+                <input type="textarea" name="external-link-desc" defaultValue="External link description"></input>
+                <button>Upload Images</button>
+                <button>Upload Videos</button>
+                <button>Upload PDFs</button>
+                
+                
+                <button onClick={() => onSubmit()}>Submit</button>
             </form>
         </>
     )
