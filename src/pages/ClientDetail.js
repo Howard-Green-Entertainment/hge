50cf9c60c6a79d516759bf650f6fa489d5e96768
Ben Parisot <me@benparisot.com>
2019-11-03 09:24:12 -0800

    clean up index.html, add new favicon

--- src/pages/ClientDetail.js
+++ src/pages/ClientDetail.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { getClient } from '../actions/client-actions';
-import './ClientDetail.css';
 
 export default function ClientDetail({ match: { params: { clientId } }}) {
 
