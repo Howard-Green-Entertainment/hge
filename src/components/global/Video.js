72ee98c79d3199ff5b9cca4d7c8f2548ca8d6a18
Ben Parisot <me@benparisot.com>
2020-02-01 15:51:45 -0800

    style Video component

--- src/components/global/Video.js
+++ src/components/global/Video.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Player } from 'video-react';
 import { getClient } from '../../actions/client-actions';
+import Loader from '../global/Loader';
 
 export default function Video({ match: { params: { clientName, videoTitle }}}) {
     const clientNames = clientName.split('-');
@@ -13,18 +14,17 @@ export default function Video({ match: { params: { clientName, videoTitle }}}) {
             .then(client => {setClient(client)})
     }, [firstName, lastName]);
     
-    if (!client) return (<p>Loading...</p>);
-    console.log('client', client);
-    console.log('video title', videoTitle);
+    if (!client) return (<Loader />);
+
     return (
         <div className="video-container">
-        <div className="video-header">
-        <h2>{client.clientFirstName} {client.clientLastName}: {videoTitle}</h2>
-        </div>
-        <Player 
-            playsInline
-            src={client.videoUrls[videoTitle]}
-        />
+            <div className="video-header">
+                <h2>{client.clientFirstName} {client.clientLastName}: <span className="client-name">{videoTitle}</span></h2>
+            </div>
+            <Player 
+                playsInline
+                src={client.videoUrls[videoTitle]}
+            />
         </div>
     )
 }
