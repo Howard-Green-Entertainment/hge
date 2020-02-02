8cdb11e1e265b1bf1f6c6f2e54becd83c233405c
Ben Parisot <me@benparisot.com>
2020-02-01 18:50:42 -0800

    add new year variable to footer

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 export default function Footer() {
+    const year = new Date().getFullYear();
     return (
         <>
             <section className="footer">
-                <p>&#9400; Howard Green Entertainment 2019</p>
+                <p>&#9400; Howard Green Entertainment {year}</p>
             </section>
         </>
     )
